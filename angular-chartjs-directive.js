1ab609895ccc1fbfcc66c8bbcb71581a51846689
Klederson Bueno <klederson@klederson.com>
2013-07-17 15:20:37 -0300

    Create angular-chartjs-directive.js

---
+++ angular-chartjs-directive.js
@@ -0,0 +1,31 @@
+angular.module("chartjs-directive", []).directive('chartjs',['$filter', function ($filter) {
+  return {
+		restrict: "E",
+		replace: true,
+		transclude: true,
+		scope: {
+			data: '=data',
+			options: '=options',
+			width : "=width",
+			height : "=height"
+		},
+		template: '<canvas></canvas>',
+		link: function ($scope, element, attr /*, ctrl */) {
+			var ctx = element[0].getContext('2d');
+
+			$scope.generate = function() {
+				$scope.instance = eval('new Chart(ctx).' +attr.isType+  '($scope.data,$scope.options)');
+			}
+
+			$scope.$watch('width',function(newValue, oldValue) {
+				element[0].width = newValue;
+				$scope.generate()
+			})
+
+			$scope.$watch('height',function(newValue, oldValue) {
+				element[0].height = newValue;
+				$scope.generate()
+			})
+		}
+	}
+}]);
